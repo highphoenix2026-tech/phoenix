d468851e74cede2a729ba350c5bba2b4436049b8
BelalAbandah <belalabandah@gmail.com>
2026-02-05 15:13:14 +0300

    testing caching7

--- app/[locale]/(root)/training/page.tsx
+++ app/[locale]/(root)/training/page.tsx
@@ -8,7 +8,6 @@ import { getAllCategoriesByLocale } from "@/app/server/categories/services";
 import { getAllCoursesByLocale } from "@/app/server/courses/services";
 import type { TranslatedCourse, TranslatedCategory,TranslatedCourseFiltered } from "@/types";
 import { TRAINING_METADATA } from "@/lib/constants/metadata"
-import { log } from "util";
 
 export const metadata=TRAINING_METADATA
 
@@ -23,20 +22,15 @@ interface PageProps {
 
 export default async function TrainingPage({ params }: PageProps) {
   const { locale } = await params;
-  console.log();
   
   const categoriesRes = await getAllCategoriesByLocale(locale);
   const coursesRes = await getAllCoursesByLocale(locale);
-
-  console.log(categoriesRes);
   console.log(" coursesRes:", coursesRes);
   console.log("locale:", locale);
   
   
-  
 
   const categoriesData: TranslatedCategory[] = categoriesRes?.data || [];
-
   const coursesData: TranslatedCourseFiltered[] = coursesRes?.data || [];
 
   return (
